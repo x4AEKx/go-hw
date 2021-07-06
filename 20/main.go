923eff2bf405bc597b4c67475a8e0df7a961ef37
Dmitry '4aek' Vlasov <4aek20@gmail.com>
2021-07-06 13:22:39 +0300

    fix

--- 20/main.go
+++ 20/main.go
@@ -6,10 +6,11 @@ func main() {
 	slice := []string{"a", "a"}
 
 	func(slice []string) {
-		slice = append(slice, "a") // slice = []string{"a", "a", "a"}
-		slice[0] = "b"             // slice = []string{"b", "a", "a"}
-		slice[1] = "b"             // slice = []string{"b", "b", "a"}
-		fmt.Print(slice)           // в этой блочной области slice = []string{"b", "b", "a"}
+		slice = append(slice, "a") // тут создается новый участок памяти на который ссылается слайс, блочной области видимости
+		slice[0] = "b"
+		slice[1] = "b"
+		fmt.Print(slice)
 	}(slice)
-	fmt.Print(slice) // тут slice = []string{"a", "a"}, т.к. глобальная область видисомти
+
+	fmt.Print(slice) // тут slice = []string{"a", "a"}, т.к. глобальная область видимости
 }
